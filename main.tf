0837ef73954c2f5b40dc6865d84a2f310fa18b6c
RLRabinowitz <arel.rabinowitz@env0.com>
2020-09-08 17:38:46 +0300

    f

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
 terraform {
-  required_version = ">= 0.12.7"
+  required_version = "0.13.1"
+}
+
+module "my_website" {
+  source = "my-website-bucket"
+
+  name = "env0.my-website.cd.blog.com"
+  html_file_source = "index.html"
+}
+
+output "my_website_endpoint" {
+  value = module.my_website.website_endpoint
 }
